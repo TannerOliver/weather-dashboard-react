d07ba212bb471a67c080a1849b14f966d5ee4b4b
TannerOliver <435.tanner@gmail.com>
2021-12-05 12:29:25 -0600

    Building out some content, Adding button and getting input to save to local storage

---
+++ client/src/components/Aside.js
@@ -0,0 +1,56 @@
+import { Nav, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { useState } from "react";
+
+export default function Aside() {
+  const [ city, setCity ] = useState('');
+  let locallyStoredCities = JSON.parse(localStorage.getItem('storedCities')) || []
+
+  const saveCityLocally = () => {
+    if (!locallyStoredCities.includes(city)) {
+      locallyStoredCities.push(city)
+      localStorage.setItem('storedCities', JSON.stringify(locallyStoredCities));
+    }
+  }
+
+  const fetchCity = async () => {
+    let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    const apiKey = '&appid=81c8f33d2835754b525076279bdd2d53';
+    console.log(city);
+    let requestUrl = apiUrl + city + apiKey
+    //  Set city in local storage
+    saveCityLocally()
+    //  Call first api call function
+    //  Append searched city
+    //  Clear the input field
+    return (
+      console.log('Hello World!')
+    )
+  }
+
+  const handleInput = ( e ) => {
+    setCity(e.target.value);
+  }
+
+  return (
+    <Nav>
+      <div className='cityInput'>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Los Angeles"
+            aria-label="currentCity"
+            aria-describedby="basic-addon"
+            onChange={handleInput}
+          />
+          <Button variant="outline-secondary" id="button-addon" onClick={fetchCity}>
+            Button
+          </Button>
+        </InputGroup>
+      </div>
+      <div>
+        <Nav.Item>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </Nav.Item>
+      </div>
+    </Nav>
+  )
+}
--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,8 +1,17 @@
+import Aside from './Aside';
+
 export default function Container() {
+
+  // In main page I want sidebar with an input and button
+  //  Inside that component I want to pull from local storage the list of previous searched cities
+  //  I will need a component for a header and Footer
+  //  seperate components for current weather and forecast
+  //  Inside of forecast component I want components that are cards for the forecast
 
   return (
     <>
-    This is a component
+      This will contain all things on webpage.
+      <Aside />
     </>
   )
 }
